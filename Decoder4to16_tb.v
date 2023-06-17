2171133e68b73f4deda0bee0074b696b076ac320
ItteRevanth <93246665+ItteRevanth@users.noreply.github.com>
2023-06-17 14:51:22 +0530

    Create Decoder4to16_tb.v

---
+++ Decoder4to16_tb.v
@@ -0,0 +1,17 @@
+//Directed testbench code for 4*16 decoder in verilog
+
+module decoder4to16_test;
+	reg [3:0] I; reg en; wire [0:15] D;
+	decoder4to16 M(.in(I),.en(en),.D(D));
+	initial
+		begin
+			$dumpfile("decoder4to16.vcd");
+			$dumpvars(0,decoder4to16_test);
+			$monitor($time,"I=%h, D=%h", I,D);
+			#5 I=4'h0; en=1'b1; 
+			#5 I=4'h6;
+			#5 I=4'ha;
+			#5 I=4'hf;
+			#5 $finish;
+		end
+endmodule
